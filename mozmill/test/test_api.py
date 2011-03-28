873029079eefcbf47f7c325183ecea7501d55459
Jeff Hammel <jhammel@mozilla.com>
2011-03-28 07:26:58 -0700

    show how to use a handler when using mozmill as an API

--- mozmill/test/test_api.py
+++ mozmill/test/test_api.py
@@ -9,9 +9,17 @@ import os
 here = os.path.dirname(os.path.abspath(__file__))
 tests = [{'path': os.path.join(here, 'test_runnershutdown.js')}]
 
-# now to do our thing
+# now to do our thing: basic run
 import mozmill
 runner = mozmill.create_runner()
 m = mozmill.MozMill(runner)
 results = m.run(tests)
 results.stop(())
+
+# this is how you use a handler
+# let's try the logging handler:
+from mozmill.logger import LoggerListener
+logger = LoggerListener()
+m = mozmill.MozMill(runner, results=results, handlers=(logger,))
+results = m.run(tests)
+results.stop((logger,))
