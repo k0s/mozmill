75e6bf6c525af06ed288024133a6f4ea77670e3e
Jeff Hammel <jhammel@mozilla.com>
2011-06-14 22:22:44 -0700

    stub a test; doesnt work

--- mutt/mutt/tests/python/testprofile.py
+++ mutt/mutt/tests/python/testprofile.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import prefs
 import subprocess
 import unittest
@@ -12,6 +14,18 @@ class ProfileTest(unittest.TestCase):
         """
         process = subprocess.Popen(args,
                                    stdout=subprocess.PIPE,
-                                   stderr=subpocess.PIPE)
+                                   stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        return stdout, stderr, process.code
+        return stdout, stderr, process.returncode
+
+    def test_basic_prefs(self):
+        prefs = {"browser.startup.homepage": "http://planet.mozilla.org/"}
+        
+        commandline = ["mozprofile"]
+        for pref, value in prefs.items():
+            commandline += ["--pref", "%s:%s" % (pref, value)]
+        profile, stderr, code = self.run(*commandline)
+        self.assertEqual(code, 0)
+
+if __name__ == '__main__':
+    unittest.main()
