1306c92f7972cc8d114ddfc0b30cc0cec82e672a
Jeff Hammel <jhammel@mozilla.com>
2011-07-01 13:32:15 -0700

    Bug 668845 - preferences must be in order to be processed correctly

--- mutt/mutt/tests/python/testprofile.py
+++ mutt/mutt/tests/python/testprofile.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 import os
-import prefs
 import shutil
 import subprocess
 import tempfile
 import unittest
-import mozprofile.prefs
-
-cast = mozprofile.prefs.Preferences.cast
+from mozprofile.prefs import Preferences
+from mozprofile.profile import Profile
 
 class ProfileTest(unittest.TestCase):
     """test mozprofile"""
@@ -36,14 +34,9 @@ class ProfileTest(unittest.TestCase):
         profile, stderr, code = self.run_command(*commandline)
         prefs_file = os.path.join(profile, 'user.js')
         self.assertTrue(os.path.exists(prefs_file))
-        read = prefs.read(prefs_file)
-        print 'PREFERENCES: %s' % _prefs
-        print prefs
+        read = Preferences.read_prefs(prefs_file)
         if isinstance(_prefs, dict):
-            print "YES, OF COURSE ITS A DICT"
             read = dict(read)
-        else:
-            print "NO IT IS NOT A DICT"
         self.assertEqual(_prefs, read)
         shutil.rmtree(profile)
 
@@ -64,7 +57,7 @@ class ProfileTest(unittest.TestCase):
         commandline = ["mozprofile"]
         for pref, value in _prefs:
             commandline += ["--pref", "%s:%s" % (pref, value)]
-        _prefs = [(i, cast(j)) for i, j in _prefs]
+        _prefs = [(i, Preferences.cast(j)) for i, j in _prefs]
         self.compare_generated(_prefs, commandline)
 
     def test_ini(self):
@@ -94,7 +87,42 @@ browser.startup.homepage = http://github.com/
         os.remove(name)
 
     def test_magic_markers(self):
-        pass
+        """ensure our magic markers are working"""
+
+        profile = Profile()
+        prefs_file = os.path.join(profile.profile, 'user.js')
+
+        # we shouldn't have any initial preferences
+        initial_prefs = Preferences.read_prefs(prefs_file)
+        self.assertFalse(initial_prefs)
+        initial_prefs = file(prefs_file).read().strip()
+        self.assertFalse(initial_prefs)
+
+        # add some preferences
+        prefs1 = [("browser.startup.homepage", "http://planet.mozilla.org/"),
+                   ("zoom.minPercent", 30)]
+        profile.set_preferences(prefs1)
+        self.assertEqual(prefs1, Preferences.read_prefs(prefs_file))
+        lines = file(prefs_file).read().strip().splitlines()
+        self.assertTrue('#MozRunner Prefs Start' in lines)
+        self.assertTrue('#MozRunner Prefs End' in lines)
+
+        # add some more preferences
+        prefs2 = [("zoom.maxPercent", 300),
+                   ("webgl.verbose", 'false')]
+        profile.set_preferences(prefs2)
+        self.assertEqual(prefs1 + prefs2, Preferences.read_prefs(prefs_file))
+        lines = file(prefs_file).read().strip().splitlines()
+        self.assertTrue(lines.count('#MozRunner Prefs Start') == 2)
+        self.assertTrue(lines.count('#MozRunner Prefs End') == 2)
+
+        # now clean it up
+        profile.clean_preferences()
+        final_prefs = Preferences.read_prefs(prefs_file)
+        self.assertFalse(final_prefs)
+        lines = file(prefs_file).read().strip().splitlines()
+        self.assertTrue('#MozRunner Prefs Start' not in lines)
+        self.assertTrue('#MozRunner Prefs End' not in lines)        
 
     def test_json(self):
         _prefs = {"browser.startup.homepage": "http://planet.mozilla.org/"}
@@ -104,8 +132,6 @@ browser.startup.homepage = http://github.com/
         fd, name = tempfile.mkstemp(suffix='.json')
         os.write(fd, json)
         os.close(fd)
-
-        print name
 
         commandline = ["mozprofile", "--preferences", name]
         self.compare_generated(_prefs, commandline)
