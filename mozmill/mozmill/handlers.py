3c0a2834ef3a18efc6d1f62f9b71b737257dd9d3
Jeff Hammel <jhammel@mozilla.com>
2011-07-21 18:25:42 -0700

    indentation cleanup

--- mozmill/mozmill/handlers.py
+++ mozmill/mozmill/handlers.py
@@ -9,55 +9,57 @@ import os
 class EventHandler(object):
   """abstract base class for handling MozMill events"""
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def __call__(self, eventName, obj):
-    """handle global events"""
+    def __call__(self, eventName, obj):
+        """handle global events"""
 
-  def events(self):
-    """returns a mapping of event types (strings) to methods"""
-    return {}
+    def events(self):
+        """returns a mapping of event types (strings) to methods"""
+        return {}
 
-  def stop(self, results, fatal):
-    """handles harness shutdown (NOT a JS event)"""
+    def stop(self, results, fatal):
+        """handles harness shutdown (NOT a JS event)"""
 
-  @classmethod
-  def add_options(cls, parser):
-    """add options to the parser"""
+    @classmethod
+    def add_options(cls, parser):
+        """add options to the parser"""
 
-class HandlerMatchException:
+
+class HandlerMatchException(object):
   """
   to be raised when inappropriate arguments are passed in to a handler;
   non-fatal command-line mismatch
   """
 
+
 def instantiate_handler(handler, options):
-  """instantiate a handler based on a set of options"""
-  try:
-    argspec = inspect.getargspec(handler.__init__)
-  except TypeError:
-    # __init__ is actually <slot wrapper '__init__' of 'object' objects>
-    # which means its not actually defined on the class
-    return handler()
-  args = argspec.args[1:] # don't need to pass self
-  defaults = argspec.defaults or []
-  offset = len(args) - len(defaults)
-  mandatory = set(args[:offset])
-  kw = dict([(args[i+offset], defaults[i])
-             for i in range(len(defaults))])
-  for arg in args:
-    if hasattr(options, arg):
-      kw[arg] = getattr(options, arg)
+    """instantiate a handler based on a set of options"""
+    try:
+        argspec = inspect.getargspec(handler.__init__)
+    except TypeError:
+        # __init__ is actually <slot wrapper '__init__' of 'object' objects>
+        # which means its not actually defined on the class
+        return handler()
+    args = argspec.args[1:] # don't need to pass self
+    defaults = argspec.defaults or []
+    offset = len(args) - len(defaults)
+    mandatory = set(args[:offset])
+    kw = dict([(args[i+offset], defaults[i])
+               for i in range(len(defaults))])
+    for arg in args:
+        if hasattr(options, arg):
+            kw[arg] = getattr(options, arg)
 
-  # ensure mandatory arguments are passed
-  if not mandatory.issubset(kw.keys()):
-    return None
+    # ensure mandatory arguments are passed
+    if not mandatory.issubset(kw.keys()):
+        return None
 
-  try:
-    return handler(**kw)
-  except HandlerMatchException:
-    return None
+    try:
+        return handler(**kw)
+    except HandlerMatchException:
+        return None
 
 def load_handler(string):
   """
@@ -77,11 +79,11 @@ def load_handler(string):
   return handler
 
 def handlers():
-  from pkg_resources import iter_entry_points
-  handlers = []
-  for i in iter_entry_points('mozmill.event_handlers'):
-    try:
-      handlers.append(i.load())
-    except:
-      raise # TODO : error handling
-  return handlers
+    from pkg_resources import iter_entry_points
+    handlers = []
+    for i in iter_entry_points('mozmill.event_handlers'):
+        try:
+            handlers.append(i.load())
+        except:
+            raise # TODO : error handling
+    return handlers
